6e590ac636ae3382cf0885102578a4a478718512
Omkar Bandikatte <149424461+omkarbandikatte@users.noreply.github.com>
2025-12-11 15:17:10 +0530

    Model Changed

--- test_career.py
+++ test_career.py
@@ -35,7 +35,7 @@ if user_input:
 
     with st.spinner("Thinking..."):
         chat_completion = groq_client.chat.completions.create(
-            model="llama3-70b-8192",
+            model="llama-3.3-70b-versatile",
             messages=st.session_state.messages,
             temperature=0.7,
             max_tokens=1024
